4888e599a7ffc1ab3fc24f4fd14450c164138287
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2023-12-06 20:32:09 +0600

    реализовал представление product_add_view

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from webapp.views import products_view, product_view
+from webapp.views import products_view, product_view, product_add_view
 
 urlpatterns = [
     path('', products_view, name='index'),
     path('products/', products_view, name='products_view'),
-    path('products/<int:pk>/', product_view, name='product_view')
+    path('products/<int:pk>/', product_view, name='product_view'),
+    path('products/add/', product_add_view, name='product_add_view')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from webapp.models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+from webapp.models import Product, Category
 
 
 def products_view(request):
@@ -10,3 +10,18 @@ def products_view(request):
 def product_view(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'product_view.html', {'product': product})
+
+
+def product_add_view(request):
+    if request.method == "GET":
+        categories = Category.objects.all()
+        return render(request, 'product_create.html', {'categories': categories})
+    elif request.method == 'POST':
+        product = Product.objects.create(
+            title=request.POST.get('title'),
+            price=request.POST.get('price'),
+            image=request.POST.get('image'),
+            description=request.POST.get('description'),
+            category_id=request.POST.get('category_id')
+        )
+        return redirect('product_view', pk=product.pk)
