5a328b3938faf17ac3896e0431ca54aa3c61cc4c
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2023-12-06 20:10:47 +0600

    реализовал представление product_view

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from webapp.views import products_view
+from webapp.views import products_view, product_view
 
 urlpatterns = [
     path('', products_view, name='index'),
-    path('products/', products_view, name='products_view')
+    path('products/', products_view, name='products_view'),
+    path('products/<int:pk>/', product_view, name='product_view')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from webapp.models import Product
 
 
 def products_view(request):
     products = Product.objects.all()
     return render(request, 'products_list.html', {'products': products})
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_view.html', {'product': product})
