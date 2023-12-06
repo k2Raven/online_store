65b2be7b0ec6d13e39950e680d785d9c26c2cce8
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2023-12-06 19:28:11 +0600

    создал модели продукта и категории

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, unique=True, verbose_name='Наименование')
+    description = models.TextField(max_length=200, null=True, blank=True, verbose_name='Описание')
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=50, unique=True, verbose_name='Наименование')
+    description = models.TextField(max_length=200, null=True, blank=True, verbose_name='Описание')
+    category = models.ForeignKey('webapp.Category', on_delete=models.CASCADE, related_name='products',
+                                 verbose_name='Категория')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время добавления')
+    price = models.DecimalField(decimal_places=2, max_digits=8, verbose_name='Стоимость')
+    image = models.URLField(verbose_name='Изображение')
