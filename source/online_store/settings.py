dc00ec7ac42136dcc63f7c0163c8a2e78a8ff9d6
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2024-04-03 20:07:40 +0600

    удалил файл .env.test изменил test.yml и settings.py

--- source/online_store/settings.py
+++ source/online_store/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', default='django-insecure-ak4y#he3xwfl%!_3i+4f6mt&$ls_(_l+kmg(nxx49&ydew3k&n')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', default=0))
